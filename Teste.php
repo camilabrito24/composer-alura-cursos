157f5534dc48083d46f41eaf924a73fd6a7e4142
Camila Brito <camilam.brito@outlook.com.br>
2024-11-29 18:55:06 -0300

    Primeiro commit

---
+++ Teste.php
@@ -0,0 +1,16 @@
+<?php
+
+class Teste
+{
+    public static function metodotestes()
+    {
+        echo "Teste";
+    }
+}
+
+/*$extensions = get_loaded_extensions();
+foreach ($extensions as $extension){
+    if(extension_loaded($extension)){
+        echo $extension . PHP_EOL;
+    }
+}*/
---
+++ src/Buscador.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Alura\BuscadorDeCursos;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+    private $httpClient;
+    private $crawler;
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function buscarCursos(string $url): array
+    {
+        $response = $this->httpClient->request('GET', $url);
+        $html = $response->getBody();
+        $this->crawler->addHtmlContent($html);
+
+        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
+        $cursos = [];
+
+        foreach ($elementosCursos as $elemento) {
+            $cursos[] = $elemento->textContent;
+        }
+
+        return $cursos;
+    }
+}
